afa8b94440190703eff8f9dbf23bcd052780a2f9
Stanislav <stas.litovchuk@mail.ru>
2024-12-26 16:32:53 +0300

    Done №3

--- Text.py
+++ Text.py
@@ -1,64 +1,42 @@
-with open("3.txt", 'r', encoding='utf-8') as third:
-    list3 = []
+from itertools import count
+
+def information(file):
     count = 0
-    for lenth in range(100):
-        data = third.readline().strip()
-        if data != "":
-            count += 1
+    list3 = []
+    with open(file, 'r', encoding='utf-8') as third:
+        for lenth in range(100):
+            data = third.readline().strip()
+            if data != "":
+                count += 1
+            else:
+                break
+            list3.append(data)
+    return [str(file), str(count), list3]
+
+with open("Done.txt", 'a', encoding='utf-8') as done:
+    for i in information("1.txt"):
+        if type(i) != list:
+            done.write(i)
+            done.write('\n')
         else:
-            break
-        list3.append(data)
-    print('1.txt')
-    print(count)
-    for l in list3:
-        print(l)
+            for o in i:
+                done.write(o)
+                done.write('\n')
 
+    for i in information("2.txt"):
+        if type(i) != list:
+            done.write(i)
+            done.write('\n')
+        else:
+            for o in i:
+                done.write(o)
+                done.write('\n')
 
-# with open("2.txt", 'r', encoding='utf-8') as second:
-#     list2 = []
-#     count = 0
-#     for lenth in range(100):
-#         data = second.readline().strip()
-#         if data != "":
-#             count += 1
-#         else:
-#             break
-#         list2.append(data)
-#     print('1.txt')
-#     print(count)
-#     for l in list2:
-#         print(l)
-
-# with open("1.txt", 'r', encoding='utf-8') as first:
-#     list1 = []
-#     count = 0
-#     for lenth in range(100):
-#         data = first.readline().strip()
-#         if data != "":
-#             count += 1
-#         else:
-#             break
-#         list1.append(data)
-#     print('1.txt')
-#     print(count)
-#     for l in list1:
-#         print(l)
-
-# with open("Done.txt", 'a', encoding='utf-8') as done:
-#     with open("1.txt", 'r', encoding='utf-8') as first:
-#         list1 = []
-#         count = 0
-#         for lenth in range(100):
-#             data = first.readline().strip()
-#             if data != "":
-#                 count += 1
-#             else:
-#                 break
-#             list1.append(data)
-#         print('1.txt')
-#         print(count)
-#         for l in list1:
-#             print(l)
-
-    # done.write(str("1.txt\n"))
-    # done.write(str(count))
+    for i in information("3.txt"):
+        if type(i) != list:
+            done.write(i)
+            done.write('\n')
+        else:
+            for o in i:
+                done.write(o)
+                done.write('\n')
